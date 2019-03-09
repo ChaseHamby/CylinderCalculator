37f63fbc20318a38b9fa37b34746e32a6d7d2b84
unknown <c.chase.hamby@gmail.com>
2019-03-09 11:51:53 -0600

    calculator works

---
+++ CylinderCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CylinderCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cylinder Calculator 1.0!");
+
+            Console.Write("Enter the cylinder's radius: ");
+            string radiusAsAString = Console.ReadLine();
+            double radius = Convert.ToDouble(radiusAsAString);
+
+            Console.Write("Enter the cylinder's height: ");
+            string heightAsAString = Console.ReadLine();
+            double height = Convert.ToDouble(heightAsAString);
+
+            double pi = 3.141592654;
+
+            double volume = pi * radius * radius * height;
+            double surfaceArea = 2 * pi * radius * (radius + height);
+
+            Console.WriteLine($"The cylinder's volume is: {volume} cubic units.");
+            Console.WriteLine($"The cylinder's surface area is: {surfaceArea} square units.");
+
+            Console.ReadKey();
+        }
+    }
+}
